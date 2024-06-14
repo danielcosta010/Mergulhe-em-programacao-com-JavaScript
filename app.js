5f0cfe848f52bd701dc2041e93bf7ae8813be603
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-06-14 20:42:49 -0300

    Numero aleatorio
    Utilizando o parseInt(Math.random() * 11)

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 alert('Boas vindas ao jogo: Número secreto')
-let numeroSecreto = 3
+let numeroSecreto = parseInt(Math.random() * 11)
+console.log(numeroSecreto);
 let chute
 let tentativas = 1
 
 // enquanto chute for diferente ao numero secreto
-while(chute != numeroSecreto) {
+while (chute != numeroSecreto) {
   chute = prompt('Digite um numero de 1 a 10')
-
-  // se chute for igual ao numero secreto exexute o 1 alert, se não execute o segundo if
   
-  if(chute == numeroSecreto) {
-    alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas}  ; )`)
-  } else {
+  // se chute for igual ao numero secreto exexute o 1 alert, se não execute o segundo if
+  if (chute == numeroSecreto) {
+    break
+    } else {
       if (chute > numeroSecreto) {
-        alert(`O numero secreto é menor que ${chute}`)
+      alert(`O numero secreto é menor que ${chute}`)
       } else {
         alert(`O número secreto é maior que ${chute}`)
-      }
-      //tentativas =  tentativas + 1
-      tentativas++
-  }
-}
+        }
+        //tentativas =  tentativas + 1
+        tentativas++
+        }
+        }
+        
+let palavraTentativa = tentativas > 1 ? 'tentativas' :  'tentativa'
+alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}  ; )`)
