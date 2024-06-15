d117d0e436bbfc956e121e8caab654bd5c01616f
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-06-15 19:58:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo: Número secreto')
-let intervaloFinal = 10 
+let intervaloFinal = 100 
 let numeroSecreto = parseInt(Math.random() * intervaloFinal + 1)
 console.log(numeroSecreto);
 let chute
@@ -8,20 +8,20 @@ let tentativas = 1
 // enquanto chute for diferente ao numero secreto
 while (chute != numeroSecreto) {
   chute = prompt(`Digite um numero de 1 a ${intervaloFinal}`)
-
   
-  // se chute for igual ao numero secreto exexute o 1 alert, se não execute o segundo if
+  // (if) se chute for igual ao número secreto pare e execute o alert fora do while, (else) se não execute o segundo if
   if (chute == numeroSecreto) {
     break
-    } else {
+  } else {
       if (chute > numeroSecreto) {
-      alert(`O numero secreto é menor que ${chute}`)
+        alert(`O numero secreto é menor que ${chute}`)
       } else {
-        alert(`O número secreto é maior que ${chute}`)
+          alert(`O número secreto é maior que ${chute}`)
         }
         //tentativas =  tentativas + 1
         tentativas++
-        }
-        }
+    }
+}
 let palavraTentativa = tentativas > 1 ? 'tentativas' :  'tentativa'
-alert(`Você acertou o número secreto era ${numeroSecreto} com ${tentativas} ${palavraTentativa}  ; )`)
+alert(`Você acertou com ${tentativas} ${palavraTentativa}, o número secreto era ${numeroSecreto}  ; )`)
+
